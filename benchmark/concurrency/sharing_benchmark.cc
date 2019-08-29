af0115a87fff8beb044bb0b54774fc2338b0b52c
Liangliang He <lliang.he@gmail.com>
2019-08-29 20:48:02 +0800

    Updatre false sharing

--- benchmark/concurrency/sharing_benchmark.cc
+++ benchmark/concurrency/sharing_benchmark.cc
@@ -15,50 +15,46 @@
 #define CACHELINE_ALIGNMENT __attribute__((aligned(CACHELINE_SIZE)))
 #endif
 
-struct CACHELINE_ALIGNMENT SharingStruct {
+struct SharingStruct {
   int a;
   int b;
 };
 
-struct CACHELINE_ALIGNMENT NoSharingStruct {
+struct NoSharingStruct {
   int a;
+  int pad[16];
   int CACHELINE_ALIGNMENT b;
 };
 
 class BM_Sharing: public benchmark::Fixture {
   public:
-    SharingStruct ss;
-    NoSharingStruct nss;
-
-    void SetUp(const ::benchmark::State& state) {}
-    void TearDown(const ::benchmark::State& state) {}
+    SharingStruct CACHELINE_ALIGNMENT ss;
+    NoSharingStruct CACHELINE_ALIGNMENT nss;
 };
 
 BENCHMARK_DEFINE_F(BM_Sharing, FalseSharing)(benchmark::State& state) {
-  int *ptr = nullptr;
   if (state.thread_index == 0) {
-    ptr = &ss.a;
+    for (auto _ : state) {
+      benchmark::DoNotOptimize(++ss.a);
+    }
   } else {
-    ptr = &ss.b;
-  }
-
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(++(*ptr));
+    for (auto _ : state) {
+      benchmark::DoNotOptimize(++ss.b);
+    }
   }
 }
 
 BENCHMARK_REGISTER_F(BM_Sharing, FalseSharing)->Threads(2);
 
 BENCHMARK_DEFINE_F(BM_Sharing, NoFalseSharing)(benchmark::State& state) {
-  int *ptr = nullptr;
   if (state.thread_index == 0) {
-    ptr = &nss.a;
+    for (auto _ : state) {
+      benchmark::DoNotOptimize(++nss.a);
+    }
   } else {
-    ptr = &nss.b;
-  }
-
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(++(*ptr));
+    for (auto _ : state) {
+      benchmark::DoNotOptimize(++nss.b);
+    }
   }
 }
 
