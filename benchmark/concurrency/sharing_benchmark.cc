8aabeb87809b67d514405800ea50f46ca885d4a8
Liangliang He <lliang.he@gmail.com>
2019-08-29 21:34:31 +0800

    Update false sharing

--- benchmark/concurrency/sharing_benchmark.cc
+++ benchmark/concurrency/sharing_benchmark.cc
@@ -7,55 +7,53 @@
 
 #include <benchmark/benchmark.h>
 
-#ifndef CACHELINE_SIZE
-#define CACHELINE_SIZE 64
-#endif
+struct SharedBuffer {
+  int paddings;
+  int32_t *buffer;
 
-#ifndef CACHELINE_ALIGNMENT
-#define CACHELINE_ALIGNMENT __attribute__((aligned(CACHELINE_SIZE)))
-#endif
+  SharedBuffer(int shards, int paddings) : paddings(paddings) {
+    int total_size = shards * sizeof(int32_t) * (1 + paddings);
+    if (posix_memalign((void**) &buffer, 64, total_size)) {
+      std::cerr << "Couldn't allocate memory" << std::endl;
+    }
+  }
 
-struct SharingStruct {
-  int a;
-  int b;
-};
+  ~SharedBuffer() {
+    free(buffer);
+  }
 
-struct NoSharingStruct {
-  int a;
-  int pad[16];
-  int CACHELINE_ALIGNMENT b;
+  inline int32_t *addr(int shard) {
+    return buffer + shard * (paddings + 1);
+  }
 };
 
 class BM_Sharing: public benchmark::Fixture {
   public:
-    SharingStruct CACHELINE_ALIGNMENT ss;
-    NoSharingStruct CACHELINE_ALIGNMENT nss;
+    std::unique_ptr<SharedBuffer> buffer;
+
+    void SetUp(const ::benchmark::State& state) {
+      int shards = state.threads;
+      int paddings = state.range(0);
+      if (state.thread_index == 0) {
+        buffer.reset(new SharedBuffer(shards, paddings));
+      }
+    }
 };
 
 BENCHMARK_DEFINE_F(BM_Sharing, FalseSharing)(benchmark::State& state) {
-  if (state.thread_index == 0) {
-    for (auto _ : state) {
-      benchmark::DoNotOptimize(++ss.a);
-    }
-  } else {
-    for (auto _ : state) {
-      benchmark::DoNotOptimize(++ss.b);
+  const int n = 1000000;
+  for (auto _ : state) {
+    for (int i = 0; i < n; ++i) {
+      buffer->addr(state.thread_index)[0]++;
     }
   }
+  state.counters["Op/s"] = benchmark::Counter(
+      static_cast<int64_t>(state.iterations()) * n,
+      benchmark::Counter::kIsRate,
+      benchmark::Counter::OneK::kIs1000);
 }
 
-BENCHMARK_REGISTER_F(BM_Sharing, FalseSharing)->Threads(2);
-
-BENCHMARK_DEFINE_F(BM_Sharing, NoFalseSharing)(benchmark::State& state) {
-  if (state.thread_index == 0) {
-    for (auto _ : state) {
-      benchmark::DoNotOptimize(++nss.a);
-    }
-  } else {
-    for (auto _ : state) {
-      benchmark::DoNotOptimize(++nss.b);
-    }
-  }
-}
-
-BENCHMARK_REGISTER_F(BM_Sharing, NoFalseSharing)->Threads(2);
+BENCHMARK_REGISTER_F(BM_Sharing, FalseSharing)->Threads(8)
+  ->DenseRange(0, 20, 2);
+BENCHMARK_REGISTER_F(BM_Sharing, FalseSharing)->Threads(16)
+  ->DenseRange(0, 20, 2);
